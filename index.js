6dcfcc3218d368cc947bb770a288d5202b07c238
Emon <emonhasan7650@gmail.com>
2023-06-06 22:57:00 +0600

    server basic express js setup done

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middleware
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
+  app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Artistry Academia Server is running....!')
+})
+
+app.listen(port, () => {
+  console.log(`Artistry Academia is listening on port ${port}`)
+})
