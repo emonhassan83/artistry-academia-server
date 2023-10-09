aa124724c8de804756627c7bf660c34240cd9c14
Emon <emonhasan7650@gmail.com>
2023-10-10 01:56:17 +0600

    user update api added

--- index.js
+++ index.js
@@ -180,6 +180,20 @@ async function run() {
       res.send(result);
     });
 
+    //Update user by email in DB 
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+
+      const result = await usersCollection.updateOne(query, updateDoc, options);
+      res.send(result);
+    });
+
 
     /*********** CLASS RELATE APIS *************/
 
